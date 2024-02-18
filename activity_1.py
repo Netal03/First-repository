9adb1e8368e561dc5e3630d11ec7df43aecf4dec
Netal03 <155095351+Netal03@users.noreply.github.com>
2024-02-18 22:50:28 +0530

    First commit

---
+++ activity_1.py
@@ -0,0 +1,6 @@
+#Print "NAMASTE DUNIYA" 10 times
+a=int(input("Enter a number:"))
+i=1
+while(i<=a):
+    print("Namaste Duniya!")
+    i=i+1
---
+++ activity_2.py
@@ -0,0 +1,5 @@
+#Print numbers form 1 to 10
+i=1
+while(i<=10):
+    print(i)
+    i=i+1
